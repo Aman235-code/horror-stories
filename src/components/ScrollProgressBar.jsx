f26c12818986b214daccd63c00ceb3e811793d33
Aman Ahamed <amanahamed04gmail.com>
2025-08-13 22:59:59 +0530

    stories add

--- src/components/ScrollProgressBar.jsx
+++ src/components/ScrollProgressBar.jsx
@@ -1,4 +1,3 @@
-// ScrollProgressBar.jsx
 import React, { useEffect, useState } from "react";
 
 const ScrollProgressBar = () => {
@@ -6,10 +5,8 @@ const ScrollProgressBar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const winScroll = document.documentElement.scrollTop;
-      const height =
-        document.documentElement.scrollHeight -
-        document.documentElement.clientHeight;
+      const winScroll = window.pageYOffset;
+      const height = document.body.scrollHeight - document.documentElement.clientHeight;
       const scrolled = (winScroll / height) * 100;
       setScrollTop(scrolled);
     };
@@ -19,7 +16,7 @@ const ScrollProgressBar = () => {
   }, []);
 
   return (
-    <div className="fixed top-[52px] left-0 w-full h-[6px] z-[998] bg-black">
+    <div className="fixed top-0 left-0 w-full h-[4px] z-[9999] bg-black">
       <div
         className="h-full bg-red-600 transition-all duration-150 ease-in-out"
         style={{ width: `${scrollTop}%` }}
